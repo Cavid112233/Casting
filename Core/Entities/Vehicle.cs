82c7b7f5eeb12f6db49891753fef3256b342e97c
Cavid112233 <suleymanli535@gmail.com>
2024-04-16 02:40:39 +0400

    Vehicle

---
+++ Core/Entities/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Brand-{Brand}:Model-{Model}: Year-{Year}");
+        }
+    }
+}
