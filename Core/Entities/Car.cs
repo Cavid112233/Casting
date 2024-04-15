f1236515c7ba650f2f44af5b010d189e702574da
Cavid112233 <suleymanli535@gmail.com>
2024-04-16 02:45:06 +0400

    TruckAndOtherclass

---
+++ Core/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Car:Vehicle
+    {
+        public int PassengerCapacity { get; set; }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Sernisin sayi: {PassengerCapacity}");
+        }
+    }
+}
---
+++ Core/Entities/RentCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class RentCar
+    {
+        public int CarLimit { get; set; }
+        public int TruckLimit { get; set; }
+        public int CarCount { get; set; }
+        public int TruckCount { get; set; }
+
+
+    }
+}
